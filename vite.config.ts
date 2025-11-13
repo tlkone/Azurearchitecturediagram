e02dd80b11a370b939e9d6cb9904e4f26bf48e92
tlkone <tainy.kone@gmail.com>
2025-11-13 14:28:42 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/Azurearchitecturediagram/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
